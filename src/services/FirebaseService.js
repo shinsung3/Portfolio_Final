6c49a68106155dc43bda0801973f89535f1bd445
조신성 <nustar1005@naver.com>
2019-07-26 11:05:16 +0900

    머지 테스트

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -72,7 +72,7 @@ export default {
         return docSnapshots.docs.map(doc => {
           let data = doc.data();
           data.created_at = new Date(data.created_at.toDate());
-          return data;
+          return data
         });
       });
   },
