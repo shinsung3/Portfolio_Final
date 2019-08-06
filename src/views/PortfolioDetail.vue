03b9aeca2318cbd2178e5318c30860f2619b4967
조신성 <nustar1005@naver.com>
2019-08-06 18:40:40 +0900

    S1P3113012-36 | 포토폴리오에서 삭제, 수정 특정사용자만 가능하도록 구현

--- src/views/PortfolioDetail.vue
+++ src/views/PortfolioDetail.vue
@@ -1,56 +1,85 @@
 <template>
-<div>
-  <!-- About Me -->
-  <v-container>
-    <v-layout my-5>
-      <v-flex xs12 sm8>
-        <center>
-          <p class="DokdoList font">
-            {{ portfolios.title }}
+  <div>
+    <!-- About Me -->
+    <v-container>
+      <v-layout my-5>
+        <v-flex xs12 sm8>
+          <center>
+            <p class="DokdoList font">
+              {{ portfolios.title }}
+            </p>
+          </center>
+          <p class="DokdoList subfont">
+            {{ portfolios.body }}
+            <br />
           </p>
-        </center>
-        <p class="DokdoList subfont">
-          {{ portfolios.body }}
-          <br />
-        </p>
-      </v-flex>
-      <v-flex pt-3 mt-5 bt-5>
-        <v-img :src="portfolios.img" aspect-ratio="1.0" />
-      </v-flex>
-    </v-layout>
-    <h1 class="DokdoList">개발 언어 : {{ portfolios.language }}</h1>
-    <h1 class="DokdoList">기간 : {{ portfolios.complete }}</h1>
-    <h1 class="DokdoList">인원 : {{ portfolios.people }}</h1>
-    <br />
-    <br />
-    <!-- 댓글 -->
-    <v-flex>
-      <v-form ref="form">
-        <v-container>
-          <v-flex>
-            <v-text-field v-model="text" label="댓글을 입력해 주세요" required>
-            </v-text-field>
-          </v-flex>
-          <v-flex>
-            <v-btn color="success" @click="insert">
-              댓글달기
-            </v-btn>
-          </v-flex>
-        </v-container>
-        <v-flex v-for="(i, j) in idcomments.length > limits ? limits : idcomments.length" :key="">
-          {{idcomments[i -1].text}}
-          {{idcomments[i -1].created_at}}
-          {{idcomments[i -1].writer}}
-          {{idcomments[i-1].fk}}
-
-          <v-btn color="red" @click="del(idcomments[i -1].id)">
-            삭제
+        </v-flex>
+        <v-flex pt-3 mt-5 bt-5>
+          <v-img :src="portfolios.img" aspect-ratio="1.0" />
+        </v-flex>
+      </v-layout>
+      <h1 class="DokdoList">개발 언어 : {{ portfolios.language }}</h1>
+      <h1 class="DokdoList">기간 : {{ portfolios.complete }}</h1>
+      <h1 class="DokdoList">인원 : {{ portfolios.people }}</h1>
+      <br />
+      <br />
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-flex
+          align-center
+          justify-end
+          row
+          fill-height
+          right
+          v-if="
+            this.$store.state.userauth == '팀원' ||
+              this.$store.state.userauth == '관리자'
+          "
+        >
+          <v-btn @click="linkToPage">
+            <v-icon>fas fa-user-edit</v-icon><span id="padding">수정</span>
+          </v-btn>
+          <v-btn @click="deleteDB" class="buttonWriter">
+            <v-icon>far fa-trash-alt</v-icon><span id="padding">삭제</span>
           </v-btn>
         </v-flex>
       </v-form>
-    </v-flex>
-  </v-container>
-</div>
+      <!-- 댓글 -->
+      <v-flex>
+        <v-form ref="form">
+          <v-container>
+            <v-flex>
+              <v-text-field
+                v-model="text"
+                label="댓글을 입력해 주세요"
+                required
+              >
+              </v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-btn color="success" @click="insert">
+                댓글달기
+              </v-btn>
+            </v-flex>
+          </v-container>
+          <v-flex
+            v-for="(i, j) in idcomments.length > limits
+              ? limits
+              : idcomments.length"
+            :key="j"
+          >
+            {{ idcomments[i - 1].text }}
+            {{ idcomments[i - 1].created_at }}
+            {{ idcomments[i - 1].writer }}
+            {{ idcomments[i - 1].fk }}
+
+            <v-btn color="red" @click="del(idcomments[i - 1].id)">
+              삭제
+            </v-btn>
+          </v-flex>
+        </v-form>
+      </v-flex>
+    </v-container>
+  </div>
 </template>
 
 <script>
