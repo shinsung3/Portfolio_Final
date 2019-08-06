f3f17f5717b24b03bd26b49927a6eb5f2d32ecb7
Help_dog <gnstjr5656@naver.com>
2019-08-06 13:19:35 +0900

    S1P3113012-81 | 포트폴리오까지만 -수정사항많음

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -136,7 +136,6 @@ export default {
           return data;
         });
       });
-      console.log(data)
   },
   getPostByIndex(id) {
     const postsCollection = firestore.collection(POSTS).doc(id);
@@ -169,6 +168,7 @@ export default {
       .doc(id)
       .delete();
   },
+
   getPortfolios() {
     const postsCollection = firestore.collection(PORTFOLIOS);
     return postsCollection
@@ -203,9 +203,9 @@ export default {
       created_at: firebase.firestore.FieldValue.serverTimestamp()
     });
   },
-  comments(id, fk, text, writer) {
-    return firestore.collection(PORTFOLIOS).doc(id).collection(COMMENTS).add({
-      id,
+  comments(portid, fk, text, writer) {
+    return firestore.collection(PORTFOLIOS).doc(portid).collection(COMMENTS).add({
+      portid,
       fk,
       text,
       writer,
@@ -225,6 +225,15 @@ export default {
         return data;
       });
     });
+  },
+  delcomment(id, fk){
+    console.log(id)
+    console.log(fk)
+    firestore
+    .collection(PORTFOLIOS)
+    .doc(id).collection(COMMENTS)
+    .doc(fk)
+    .delete();
   },
 	getAuthorization() {
 		const userauthCollection = firestore.collection(USERAUTH)
