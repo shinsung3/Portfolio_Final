1e59b22b75bd500125a59efe6f9efc46177022e9
KHI <kimhi297@gmail.com>
2019-08-06 13:50:02 +0900

    S1P3113012-51 | 쿠키를 허용한 유저에게 글(포스트, 포트폴리오) 등록 시 푸시 알람을 보냄.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require ('firebase-admin');
+admin.initializeApp();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,7 +9,6 @@ const functions = require('firebase-functions');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("테스트용 확인");
-
 });
 
 exports.logincheck = functions.https.onCall((data, context) => {
@@ -25,3 +26,54 @@ const uid = context.auth.uid;
   console.log(email+"로그아웃")
   return {email};
 });
+
+exports.portflioNotification = functions.firestore
+  .document("portfolios/{portfolios}")
+  .onCreate(async event => {
+     var msg = "New Portfolio : " + event.data().title;
+     console.log(msg);
+     return pushMessage(msg);
+  });
+
+exports.sendPostNotification = functions.firestore
+  .document("posts/{posts}")
+  .onCreate(async event => {
+     var msg = "New Post : " + event.data().title;
+     console.log(msg);
+     return pushMessage(msg);
+  });
+
+function pushMessage(msg) {
+  var payload = {
+    notification: {
+      title: msg
+    }
+};
+
+const users = admin.firestore().collection("userauth");
+
+users
+ .get()
+ .then(snapshot => {
+   snapshot.forEach(doc => {
+     if (doc.data().deviceToken !== "") {
+       console.log(doc.data().deviceToken);
+       token = doc.data().deviceToken;
+       console.log("전송 : ", token);
+       admin.messaging().sendToDevice(token, payload)
+         .then(function(response) {
+           console.log('메시지 전송 성공:', response);
+         })
+         .catch(function(error) {
+           console.log('메시지 전송 실패:', error);
+         });
+     } else {
+       console.log(doc.data().GRADE);
+     }
+   });
+   return "모든 유저에게 메시지 전송";
+ })
+ .catch(err => {
+   console.log("문서를 얻어오는데 실패했습니다", err);
+ });
+}
