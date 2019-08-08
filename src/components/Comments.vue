734f06f0f2fdb55e84aecd973506ff2a3b6ae453
nnnangni <yud02150@gmail.com>
2019-08-08 10:08:39 +0900

    S1P3113012-89 | 댓글창 수정

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,27 +1,24 @@
 <template>
-<div>
-  <v-layout>
-    <v-flex>
+  <v-container>
+    <v-layout>
       <v-form ref="form">
-        <v-container>
-          <v-flex>
+        <v-flex>
+          <v-div>
             <v-text-field v-model="text" label="댓글을 입력해 주세요" required>
             </v-text-field>
-          </v-flex>
-          <v-flex>
+          </v-div>
+          <v-div>
             <v-btn color="success" @click="insert">
               댓글달기
             </v-btn>
-          </v-flex>
-        </v-container>
+          </v-div>
+        </v-flex>
 
         <v-flex v-for="(i, j) in idcomments.length > limits
             ? limits
             : idcomments.length" :key="j">
 
-          <v-card
-            class="mx-auto"
-          >
+          <div>
             <v-list-item>
               <v-list-item-content v-if="idcomments[i - 1].portid == thisid">
                 <div class="overline"><v-chip class="ma-2"><strong>{{ idcomments[i - 1].writer }}</strong></v-chip> {{ idcomments[i - 1].created_at | moment("YYYY년 MM월 DD일")}}</div>
@@ -30,16 +27,16 @@
             </v-list-item>
 
             <v-card-actions v-if="idcomments[i - 1].writer == thislogin">
-              <v-btn text @click="del(idcomments[i - 1].id)">삭제</v-btn>
-              <v-btn text @click="함수명(idcomments[i - 1].id)">수정</v-btn>
+              <v-btn @click="del(idcomments[i - 1].id)">삭제</v-btn>
+              <v-btn @click="함수명(idcomments[i - 1].id)">수정</v-btn>
             </v-card-actions>
-          </v-card>
+          </div>
+
         </v-flex>
 
       </v-form>
-    </v-flex>
-  </v-layout>
-</div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -104,3 +101,9 @@ export default {
 
 }
 </script>
+
+<style media="screen">
+  .comments {
+    border-color: transparent!important;
+  }
+</style>
