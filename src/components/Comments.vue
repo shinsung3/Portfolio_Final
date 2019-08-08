a92bd08c730560d6f472ec7567fc1ae73f6d3c22
nnnangni <yud02150@gmail.com>
2019-08-08 10:42:27 +0900

    S1P3113012-89 | 댓글 달기 창 수정

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,17 +1,15 @@
 <template>
   <v-container>
     <v-layout>
-      <v-form ref="form">
+      <v-content>
         <v-flex>
-          <v-div>
-            <v-text-field v-model="text" label="댓글을 입력해 주세요" required>
+          <v-row>
+            <v-text-field v-model="text" append-icon="message" label="댓글을 입력해 주세요" required>
             </v-text-field>
-          </v-div>
-          <v-div>
-            <v-btn color="success" @click="insert">
-              댓글달기
+            <v-btn flat icon @click="insert">
+              <v-icon>add_comment</v-icon>
             </v-btn>
-          </v-div>
+          </v-row>
         </v-flex>
 
         <v-flex v-for="(i, j) in idcomments.length > limits
@@ -22,19 +20,23 @@
             <v-list-item>
               <v-list-item-content v-if="idcomments[i - 1].portid == thisid">
                 <div class="overline"><v-chip class="ma-2"><strong>{{ idcomments[i - 1].writer }}</strong></v-chip> {{ idcomments[i - 1].created_at | moment("YYYY년 MM월 DD일")}}</div>
-                <v-list-item-subtitle class="ma-3">{{ idcomments[i - 1].text }}</v-list-item-subtitle>
+                <v-list-item-subtitle class="ma-4">{{ idcomments[i - 1].text }}</v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
 
             <v-card-actions v-if="idcomments[i - 1].writer == thislogin">
-              <v-btn @click="del(idcomments[i - 1].id)">삭제</v-btn>
-              <v-btn @click="함수명(idcomments[i - 1].id)">수정</v-btn>
+              <v-btn flat icon @click="del(idcomments[i - 1].id)">
+                <v-icon>close</v-icon>
+              </v-btn>
+              <v-btn flat icon @click="함수명(idcomments[i - 1].id)">
+                <v-icon>edit</v-icon>
+              </v-btn>
             </v-card-actions>
           </div>
 
         </v-flex>
 
-      </v-form>
+      </v-content>
     </v-layout>
   </v-container>
 </template>
